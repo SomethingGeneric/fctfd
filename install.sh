8a25ecde8a4ac1f3bf0822581c8919a21c38f2c6
Matt C <matthew.compton@mymail.champlain.edu>
2025-02-21 15:14:14 -0500

    sudo for all the stuff we need to run

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # git pull
 
-[[ -f /etc/systemd/system/ctfscoreboard.service ]] && systemctl disable --now ctfscoreboard && rm /etc/systemd/system/ctfscoreboard.service
+[[ -f /etc/systemd/system/ctfscoreboard.service ]] && sudo systemctl disable --now ctfscoreboard && sudo rm /etc/systemd/system/ctfscoreboard.service
 
 [[ ! -d venv ]] && python3 -m venv venv
 ./venv/bin/pip install -r requirements.txt
@@ -14,9 +14,9 @@ cp ctfscoreboard.service.ex ctfscoreboard.service
 sed -i "s/MYPWD/${my_pwd//\//\\/}/g" ctfscoreboard.service
 sed -i "s/UNAME/${user}/g" ctfscoreboard.service
 
-cp ctfscoreboard.service /etc/systemd/system/ctfscoreboard.service
-systemctl daemon-reload
-systemctl enable --now ctfscoreboard
+sudo cp ctfscoreboard.service /etc/systemd/system/ctfscoreboard.service
+sudo systemctl daemon-reload
+sudo systemctl enable --now ctfscoreboard
 
 if [[ ! -d db ]]; then
     ./venv/bin/python3 create_data.py
